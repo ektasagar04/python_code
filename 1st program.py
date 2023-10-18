7892b5d52dc59a65cfd7c5d613b0da86ec0369f0
ektasagar04 <147904656+ektasagar04@users.noreply.github.com>
2023-10-18 20:21:32 +0530

    Add files via upload

---
+++ 1st program.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ektas
+#
+# Created:     15/10/2023
+# Copyright:   (c) ektas 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+print("hello world")
+print("hello coder")
+
+print("i know haow to count...1,2,3,4,....")
