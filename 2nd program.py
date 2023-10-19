abc2142dc2725dbedfd965fd3e65860b4f704c6b
ektasagar04 <147904656+ektasagar04@users.noreply.github.com>
2023-10-19 09:23:20 +0530

    Add files via upload

---
+++ 2nd program.py
@@ -0,0 +1,40 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ektas
+#
+# Created:     15/10/2023
+# Copyright:   (c) ektas 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+print("hello world")
+print("hello coder")
+
+print("i know haow to count...1,2,3,4,....")
+
+name = "ekta"
+age = 19
+print(name)
+print(age)
+
+first_name = "tony"
+second_name = "stark"
+age = 51
+is_genius = True
+print(first_name)
+print(second_name)
+print(age)
+
+name = input("What is your name? ")
+print("hello " + name)
+
+superhero = input("who is your superhero name ?")
+print(superhero)
+
+
+
+old_age = input("enter your old age : ")
+
+
