c813791fc5b8157715d6fa6c308129fd1ceb8195
ektasagar04 <147904656+ektasagar04@users.noreply.github.com>
2023-10-20 22:26:58 +0530

    Add files via upload

---
+++ 03 program.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ektas
+#
+# Created:     18/10/2023
+# Copyright:   (c) ektas 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+old_age = input("enter your old age :")
+new_age = int(old_age) + 4
+print(new_age)
+
+
+number = 18
+print(float(18))
