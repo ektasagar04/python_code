f66f30173f3a307d5151e2353f6816207722dcf7
ektasagar04 <147904656+ektasagar04@users.noreply.github.com>
2023-10-21 09:45:51 +0530

    Add files via upload

---
+++ 04 program.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ektas
+#
+# Created:     18/10/2023
+# Copyright:   (c) ektas 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+number = 18
+print(float(18))
+
+first = input("enter first number:")
+second = input("enter second number:")
+sum = int(first) + int(second)
+print("the sum is :"+ str(sum))
+
