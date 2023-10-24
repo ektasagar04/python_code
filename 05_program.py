8271b5e4849e705117a7d9063c71bd231704bf4c
ektasagar04 <147904656+ektasagar04@users.noreply.github.com>
2023-10-24 19:44:20 +0530

    Add files via upload

---
+++ 05_program.py
@@ -0,0 +1,36 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ektas
+#
+# Created:     18/10/2023
+# Copyright:   (c) ektas 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+name = "Tony stark"
+
+print(name.upper())
+
+print(name.lower())
+
+print(name.find('s'))
+
+print(name.find('S'))
+
+print(name.find('stark'))
+
+print(name.replace("Tony stark", "ironman"))
+
+print(name.replace("Tony stark", "ironman"))
+print(name)
+
+print(name.replace("T",("m")))
+
+print("T" in name)
+print("m" in name)
+
+print("stark" in name)
+print("ironman" in name)
+
