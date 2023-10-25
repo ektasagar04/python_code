3161fd8a455d01872c79b024c6766a98731f2270
ektasagar04 <147904656+ektasagar04@users.noreply.github.com>
2023-10-25 07:50:26 +0530

    Add files via upload

---
+++ 06_program.py
@@ -0,0 +1,28 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ektas
+#
+# Created:     18/10/2023
+# Copyright:   (c) ektas 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+print(5 + 2)
+print(5 - 2)
+print(5 * 2)
+print(5 / 2)
+print(5 // 2)
+print(5 % 2)
+print(5 ** 2)
+
+result = 2 + 3 * 5
+print(result)
+
+i = 5
+i = i + 2
+print(i)
+
+result = (2 + 3) * 5
+print(result)
